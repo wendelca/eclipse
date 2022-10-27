3304624c92ad46e86f7e74b167193654902f084f
wendelca <wendelca@gmail.com>
2022-10-27 11:56:00 -0300

    Adicionei arquivos via upload

---
+++ Hello Github/src/br/com/wendelca/git/HellGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.wendelca.git;
+
+/**
+ * @author wendel 
+ * eclipse com github
+ *
+ */
+public class HellGit {
+
+	
+	public static void main(String[] args) {
+	System.out.println("Hello Git e GitHub");
+
+	}
+
+}
